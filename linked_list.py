73bf1c90b34afde4b4a52620114a43ef66b9d240
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-12-11 19:58:47 +0000

    LinkedList solution

--- linked_list.py
+++ linked_list.py
@@ -1,6 +1,24 @@
+from node import Node
+
+
 class LinkedList:
+    def __init__(self):
+        self.head_node = None
+
     def append(self, value):
-        pass
+        if self.head_node is None:
+            self.head_node = Node(value, None)
+        current_node = self.head_node
+        while True:
+            if current_node.next() is None:
+                current_node.next_value = Node(value, None)
+                return
+            current_node = current_node.next()
 
     def length(self):
-        pass
+        length_so_far = 0
+        current_node = self.head_node
+        while current_node is not None:
+            length_so_far += 1
+            current_node = current_node.next()
+        return length_so_far - 1
