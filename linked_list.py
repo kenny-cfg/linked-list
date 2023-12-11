a931408ef4ea24b3fce5bbd120102133987dab6f
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-12-11 20:20:35 +0000

    Some type annotations

--- linked_list.py
+++ linked_list.py
@@ -1,18 +1,18 @@
 from node import Node
 
 
-class LinkedList:
+class LinkedList[T]:
     def __init__(self):
         self.head_node = None
 
-    def append(self, value):
+    def append(self, value: T) -> None:
         new_node = Node(value, None)
         if self.head_node is None:
             self.head_node = Node(value, None)
             return
         self.head_node.append(new_node)
 
-    def length(self):
+    def length(self) -> int:
         if self.head_node is None:
             return 0
         return self.head_node.length()
--- node.py
+++ node.py
@@ -1,18 +1,21 @@
-class Node:
-    def __init__(self, data, next_value):
+from typing import Optional
+
+
+class Node[T]:
+    def __init__(self, data: T, next_value: Optional):
         self.data = data
         self.next_value = next_value
 
-    def next(self):
+    def next(self) -> T:
         return self.next_value
 
-    def append(self, node):
+    def append(self, node) -> None:
         if self.next_value is None:
             self.next_value = node
         else:
             self.next_value.append(node)
 
-    def length(self):
+    def length(self) -> int:
         if self.next_value is None:
             return 1
         return 1 + self.next_value.length()
